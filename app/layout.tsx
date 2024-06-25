664e2dd08327739092900cab76f7ca94153dec8f
elinoza <hillcakmak@gmail.com>
2024-06-25 23:44:48 +0300

    github icon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SLOT MACHINE",
+  title: "Slot Machine",
   description: "Spin for a motivational quote win!",
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 import Slot from "../components/Slot";
+import { IoLogoGithub } from "react-icons/io";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center  p-24">
+      <span className="absolute top-5 right-10 text-2xl">
+        <a href="https://github.com/elinoza/slot-machine" target="_blank">
+          <IoLogoGithub />
+        </a>
+      </span>
       <Slot />
     </main>
   );
